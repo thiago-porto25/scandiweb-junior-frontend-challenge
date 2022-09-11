a901c0cd3a60b38e2da4d373913430b00f47d357
Thiago Porto <thiago.porto@d3.do>
2022-09-11 10:58:41 -0300

    feat: Create Button component

---
+++ src/shared/components/Button/index.tsx
@@ -0,0 +1,62 @@
+import styled, { css } from 'styled-components'
+
+interface IStyledButtonProps {
+  variant?: 'primary' | 'secondary'
+  round?: boolean
+  smallText?: boolean
+  selected?: boolean
+  children: React.ReactNode
+}
+
+export const Button = styled.button<IStyledButtonProps>`
+  ${({
+    theme,
+    variant = 'primary',
+    round = false,
+    smallText = false,
+    selected = false,
+  }) => css`
+    width: 100%;
+    height: 100%;
+    border-radius: ${round ? theme.borderRadius.round : 0};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
+    user-select: none;
+    cursor: pointer;
+    outline: none;
+    transition: background-color 130ms ease, border-color 130ms ease;
+    ${smallText
+      ? theme.textStyles.buttonSmall
+      : theme.textStyles.buttonRegular};
+    background-color: ${theme.colors.brand.primary};
+    border: 1px solid ${theme.colors.brand.primary};
+
+    &:hover {
+      background-color: ${theme.colors.brand.primaryHighlight};
+      border: 1px solid ${theme.colors.brand.primaryHighlight};
+    }
+
+    ${variant === 'secondary' &&
+    css`
+      background-color: ${theme.colors.neutral.light};
+      border: 1px solid ${theme.colors.neutral.dark};
+      color: ${theme.colors.neutral.dark};
+
+      &:hover {
+        background-color: ${theme.colors.neutral.dark};
+        border: 1px solid ${theme.colors.neutral.dark};
+        color: ${theme.colors.neutral.light};
+      }
+
+      ${selected &&
+      css`
+        background-color: ${theme.colors.neutral.dark};
+        border: 1px solid ${theme.colors.neutral.dark};
+        color: ${theme.colors.neutral.light};
+        pointer-events: none;
+      `}
+    `}
+  `}
+`
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,4 @@
+export { Button } from './Button'
 export { Container } from './Container'
 export { Spinner } from './Spinner'
 export { Typography } from './Typography'
