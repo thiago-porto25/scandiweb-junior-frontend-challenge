2375d64d7ba1cbc2371ac9cf632615fdbe193701
Thiago Porto <thiago.porto@d3.do>
2022-09-12 21:33:21 -0300

    feat: initial setup for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { GlobalLayout } from './shared/layouts'
 
@@ -6,6 +7,16 @@ class App extends React.Component {
   render(): React.ReactNode {
     return (
       <GlobalLayout>
+        <Routes>
+          <Route path='/' element={<div>category</div>}>
+            <Route
+              path='category/:categoryName'
+              element={<div>category</div>}
+            />
+          </Route>
+
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
         <div />
       </GlobalLayout>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { client } from '@tilework/opus'
 import { ThemeProvider } from 'styled-components'
 
@@ -23,7 +24,9 @@ root.render(
       <Provider store={store}>
         <PersistGate loading={<LoadingLayout />} persistor={persistor}>
           <ErrorBoundary>
-            <App />
+            <Router>
+              <App />
+            </Router>
           </ErrorBoundary>
         </PersistGate>
       </Provider>
