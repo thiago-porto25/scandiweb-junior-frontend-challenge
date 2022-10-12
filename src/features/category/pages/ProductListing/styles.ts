d0fcc624c6ce138f0dcdd3bd7c3b127406117652
Thiago Porto <thiago.porto@d3.do>
2022-10-12 11:21:44 -0300

    fix: fix double requests

--- src/features/category/pages/ProductListing/styles.ts
+++ src/features/category/pages/ProductListing/styles.ts
@@ -9,7 +9,8 @@ export const HeadingContainer = styled.div`
 export const ProductList = styled.ul`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(386px, 1fr));
-  place-items: end;
+  justify-items: center;
+  align-items: end;
   row-gap: 103px;
   column-gap: 40px;
   margin-bottom: 80px;
